d29f375a812ba557f9d60f1e3d1e51e70de0fda2
Margina757 <Margina757>
2014-03-12 17:21:35 +0800

    indexFrom

---
+++ index.php
@@ -0,0 +1,15 @@
+<form name="payFormCcard" method="post" action="https://www.paydollar.com/b2c2/eng/payment/payForm.jsp">
+<input type="hidden" name="merchantId" value="1">
+<input type="hidden" name="amount" value="3000.0" >
+<input type="hidden" name="orderRef" value="000000000014">
+<input type="hidden" name="currCode" value="344" >
+<input type="hidden" name="mpsMode" value="NIL" >
+<input type="hidden" name="successUrl" value="http://www.yourdomain.com/Success.html">
+<input type="hidden" name="failUrl" value="http://www.yourdomain.com/Fail.html">
+<input type="hidden" name="cancelUrl" value="http://www.yourdomain.com/Cancel.html">
+<input type="hidden" name="payType" value="N">
+<input type="hidden" name="lang" value="E">
+<input type="hidden" name="payMethod" value="ALL">
+<input type=”hidden” name=”secureHash” value=”44f3760c201d3688440f62497736bfa2aadd1bc0”>
+<input type="submit" name="submit">
+</form>
